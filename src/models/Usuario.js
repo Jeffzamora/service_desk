af4d1f454b63049c6eca4ea295792f9fc2d1e04c
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-03-12 18:59:47 -0600

    Update Usuario.js
    Correo de usuario agregado

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -8,7 +8,12 @@ const usuarioSchema = mongoose.Schema({
     usu_pass: {
         type: String,
         required: true
+    },
+    usu_correo: {
+        type: String,
+        required: false
     }
+      
 });
 
 module.exports=mongoose.model('users',usuarioSchema);
